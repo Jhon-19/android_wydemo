3bbbff22351344175bbeb5d1197103f5a0bd4c86
sgsg <519117694@qq.com>
2022-01-27 21:05:29 +0800

    调用相机和手机相册获取图片

--- sgsg/WyDemo/app/src/main/java/com/example/wydemo/TestActivity.kt
+++ sgsg/WyDemo/app/src/main/java/com/example/wydemo/TestActivity.kt
@@ -1,31 +1,132 @@
 package com.example.wydemo
 
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_test.*
 import android.app.Dialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.os.Environment.*
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class TestActivity : AppCompatActivity() {
+    //网络请求
+    private var relaAddress = "/whu/lectureList"
+    private val args = HashMap<String, String>()
+
+    //图片放大显示
     private lateinit var dialog: Dialog
     private lateinit var image: ImageView
 
-    private var relaAddress = "/whu/lectureList"
-    private val args = HashMap<String, String>()
+    //相机和相册获取图片
+    val takePhoto = 1
+    val fromAlbum = 2
+    lateinit var imageUri: Uri
+    lateinit var outputImage: File
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
 
         init()
-        testBtn1.setOnClickListener {}
-        imageView.setOnClickListener {
-            dialog?.show()
+        testBtn1.setOnClickListener {
+            // 创建File对象，用于存储拍照后的图片
+            outputImage = File(externalCacheDir, "output_image.jpg")
+            if (outputImage.exists()) {
+                outputImage.delete()
+            }
+            outputImage.createNewFile()
+            imageUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                FileProvider.getUriForFile(this,
+                    "com.example.wydemo.fileprovider", outputImage)
+            } else {
+                Uri.fromFile(outputImage)
+            }
+            // 启动相机程序
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            startActivityForResult(intent, takePhoto)
+        }
+        testBtn2.setOnClickListener {
+            // 打开文件选择器
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            // 指定只显示图片
+            intent.type = "image/*"
+            startActivityForResult(intent, fromAlbum)
         }
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            takePhoto -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    // 将拍摄的照片显示出来
+                    val bitmap =
+                        BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri))
+                    testImage.setImageBitmap(rotateIfRequired(bitmap))
+                }
+            }
+            fromAlbum -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    data.data?.let { uri ->
+                        // 将选择的图片显示
+                        Log.d("sgsg",uri.toString())
+                        val bitmap = getBitmapFromUri(uri)
+                        testImage.setImageBitmap(bitmap)
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun getBitmapFromUri(uri: Uri) = contentResolver
+        .openFileDescriptor(uri, "r")?.use {
+            BitmapFactory.decodeFileDescriptor(it.fileDescriptor)
+        }
+
+    private fun rotateIfRequired(bitmap: Bitmap): Bitmap {
+        val exif = ExifInterface(outputImage.path)
+        val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_NORMAL)
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90)
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180)
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270)
+            else -> bitmap
+        }
+    }
+
+    private fun rotateBitmap(bitmap: Bitmap, degree: Int): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degree.toFloat())
+        val rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height,
+            matrix, true)
+        bitmap.recycle() // 将不再需要的Bitmap对象回收
+        return rotatedBitmap
+    }
+
 
     private fun init() {
 
@@ -37,5 +138,6 @@ class TestActivity : AppCompatActivity() {
 
         image.setOnClickListener { dialog.dismiss() }
     }
+
 
 }
